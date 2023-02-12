97f6fe398269e4ce06b1050a3bb5a0b8f228b6b1
yonggang-Xie <74223059+yonggang-Xie@users.noreply.github.com>
2023-02-12 16:06:53 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,19 +15,20 @@ version = '0.1.0'
 
 # -- General configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-]
-
 # extensions = [
-#     'sphinx.ext.duration',
-#     'sphinx.ext.doctest',
 #     'sphinx.ext.autodoc',
-#     'sphinx.ext.autosummary',
-#     'sphinx.ext.intersphinx',
-#     'recommonmark',    
+#     'sphinx.ext.viewcode',
 # ]
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'recommonmark', 
+    'myst_parser',
+]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
