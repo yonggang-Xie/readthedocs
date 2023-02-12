81555f541b97cd42a26454ec4e99df27a5c36c6a
yonggang-Xie <74223059+yonggang-Xie@users.noreply.github.com>
2023-02-13 00:47:39 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,24 +15,18 @@ version = '0.1.0'
 
 # -- General configuration
 
-# extensions = [
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.viewcode',
-# ]
+
+
+
 
 extensions = [
-    'sphinx.ext.githubpages',
-    'myst_parser'
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+     'myst_parser',
 ]
-
-# extensions = [
-#     'sphinx.ext.duration',
-#     'sphinx.ext.doctest',
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.autosummary',
-#     'sphinx.ext.intersphinx',
-#      'myst_parser',
-# ]
 
 source_suffix = ['.rst', '.md']
 
